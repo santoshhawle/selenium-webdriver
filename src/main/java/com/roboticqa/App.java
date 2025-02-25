5901fa04cd805e417b3a00fda89157f71e8000fe
santoshhawle <santoshmh90@gmail.com>
2025-02-25 11:14:11 +0530

    Maven project with selenium dependency added

---
+++ src/main/java/com/roboticqa/App.java
@@ -0,0 +1,13 @@
+package com.roboticqa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
